0b5006f3bdc26c3561cc52fc0051562c12ffa3d8
Corentin Bringer <corentinbringer@ik.me>
2024-12-01 15:39:28 +0100

    web page + controller and service v0.1

--- src/main/java/fr/corentinbringer/sudoku_generator/model/Sudoku.java
+++ src/main/java/fr/corentinbringer/sudoku_generator/model/Sudoku.java
@@ -1,10 +1,12 @@
 package fr.corentinbringer.sudoku_generator.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Sudoku {
 
     private int[][] grid;
--- src/main/java/fr/corentinbringer/sudoku_generator/service/SudokuService.java
+++ src/main/java/fr/corentinbringer/sudoku_generator/service/SudokuService.java
@@ -1,4 +1,17 @@
 package fr.corentinbringer.sudoku_generator.service;
 
+import fr.corentinbringer.sudoku_generator.model.Sudoku;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SudokuService {
+
+    private final SudokuGenerator sudokuGenerator;
+
+    public Sudoku generateSudoku(String difficulty) {
+        int[][] grid = sudokuGenerator.generateGrid(difficulty);
+        return new Sudoku(grid, difficulty);
+    }
 }
