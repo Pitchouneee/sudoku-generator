c76ee6156d25724a980fbc672263c98c47c70a10
Corentin Bringer <corentinbringer@ik.me>
2024-12-01 15:36:47 +0100

    web page + controller

--- src/main/java/fr/corentinbringer/sudoku_generator/controller/SudokuController.java
+++ src/main/java/fr/corentinbringer/sudoku_generator/controller/SudokuController.java
@@ -1,4 +1,30 @@
 package fr.corentinbringer.sudoku_generator.controller;
 
+import fr.corentinbringer.sudoku_generator.model.Sudoku;
+import fr.corentinbringer.sudoku_generator.service.SudokuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
 public class SudokuController {
+
+    private final SudokuService sudokuService;
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/sudoku")
+    public String generateSudoku(@RequestParam(defaultValue = "moyen") String difficulty,
+                                 Model model) {
+        Sudoku sudoku = sudokuService.generateSudoku(difficulty);
+        model.addAttribute("sudoku", sudoku);
+
+        return "sudoku";
+    }
 }
