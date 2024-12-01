edf906d4218abd69b2ed59a2a3d083fe9702d84a
Corentin Bringer <corentinbringer@ik.me>
2024-12-01 15:42:26 +0100

    structure for generator class

--- src/main/java/fr/corentinbringer/sudoku_generator/service/SudokuGenerator.java
+++ src/main/java/fr/corentinbringer/sudoku_generator/service/SudokuGenerator.java
@@ -1,4 +1,26 @@
 package fr.corentinbringer.sudoku_generator.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SudokuGenerator {
+
+    public int[][] generateGrid(String difficulty) {
+        int[][] grid = new int[9][9];
+        fillGrid(grid);
+        removeNumbers(grid, difficulty);
+        return grid;
+    }
+
+    private void fillGrid(int[][] grid) {
+
+    }
+
+    private void removeNumbers(int[][] grid, String difficulty) {
+
+    }
+
+    private boolean isValid(int[][] grid, int row, int col, int num) {
+        return true;
+    }
 }
