3e99a71ff7e572b5545add2be1b87f5556c400b2
Corentin Bringer <corentinbringer@ik.me>
2024-12-01 15:22:31 +0100

    first commit

---
+++ src/test/java/fr/corentinbringer/sudoku_generator/SudokuGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.corentinbringer.sudoku_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
